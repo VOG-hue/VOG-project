bd9cc9abed39032b1b2300ba0f2171d29f515860
VOG-hue <kadaouiadekadaouiadel00@gmail.com>
2025-06-11 01:30:28 +0200

    Mise à jour du code HTML/CSS/JS

--- server/index.js
+++ server/index.js
@@ -5,13 +5,16 @@ const app = express();
 // Sert les fichiers statiques depuis /publique
 app.use(express.static(path.join(__dirname, '../publique')));
 
+// Configuration de EJS
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../views'));
 
+// Route principale
 app.get('/', (req, res) => {
   res.render('index');
 });
 
+// Lancer le serveur
 app.listen(3000, () => {
   console.log('✅ Server running on http://localhost:3000');
 });
